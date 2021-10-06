80de0c79046974ffb46cab0541aaa819d8699608
JSmith <65369149+JHussle@users.noreply.github.com>
2021-10-06 11:36:10 -0500

    working with operators, strings, interpolation

--- hello.rb
+++ hello.rb
@@ -1,1 +1,62 @@
+# print Hello, World to the console
 puts "Hello, World!"
+
+=begin
+    This is 
+    my first 
+    multiline
+    comment
+    
+=end
+puts "The lines 3-10"
+puts "has a multiline"
+puts "comment."
+
+# outputs the value of the variable myVar
+my_var = 5
+puts my_var
+
+# variable sum contains the sum of variable x and y
+x = 5
+y = 15
+sum = x + y
+puts sum
+
+# variable data types
+name = "John"
+age = 25
+puts "Your name is #{name} and you are #{age} years old"
+
+# operators
+a = 5.0
+b = 3
+
+# addition
+sum = a + b
+puts sum
+
+# subtraction
+sub = a - b
+puts sub
+
+# multiplication
+mul = a * b
+puts mul
+
+# division
+div = a / b
+puts div
+
+# mod
+mod = a % b
+puts mod
+
+# String Interpolation
+name = "Jane"
+age = 21
+puts "Your name is #{name} and you are #{age} years old"
+
+# String Concatenation
+string_1 = "Hello, "
+string_2 = 'World'
+puts string_1 + string_2
